27b95102a6e1334dc90008b1f90047ed4eb9b24d
Thomas Engels <thomas.engels@keysight.com>
2025-01-10 14:53:06 -0700

    Experiment w/ tripled array

--- main.cpp
+++ main.cpp
@@ -8,13 +8,19 @@ void assign_questions(
 {
     int num_questions = std::size(questions);
     int num_names = std::size(names);
+
+
+
     for (int i = 0; i < num_questions; i++)
     {
-        for (int j = 0; j < 3; j++)
-        {
-            const std::string& assigned_name = names[(i + j) % num_names];
-            q_assignments_map[assigned_name].push_back(questions[i]);
-        }
+        std::string & name = names[i % num_names];
+        q_assignments_map[name].push_back(questions[i]);
+        // for (int j = 0; j < 3; j++)
+        // {
+        //     std::string & assigned_name = names[j % num_names];
+        //     std::cout << " " << assigned_name << std::endl;
+        //     q_assignments_map[assigned_name].push_back(questions[i]);
+        // }
     }
 }
 
@@ -35,9 +41,8 @@ void display_assignments(
 
 int main()
 {
-
-    std::vector<int> intermediate_questions = {3, 4, 8, 9};
-    std::vector<int> basic_questions = {1, 2, 5, 6, 7};
+    std::vector<int> intermediate_questions = {3, 3, 3, 4, 4, 4, 8, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 11};
+    // std::vector<int> basic_questions = {1, 2, 5, 6, 7, 10};
     std::vector<std::string> names = {"Ally", "Marcus", "Julia", "Max", "Tommy"};
     std::map<std::string, std::vector<int>> q_assignments_map;
     for (const auto & name : names)
@@ -45,7 +50,7 @@ int main()
         q_assignments_map[name];
     }
 
-    assign_questions(basic_questions, q_assignments_map, names);
+    // assign_questions(basic_questions, q_assignments_map, names);
     assign_questions(intermediate_questions, q_assignments_map, names);
 
     display_assignments(q_assignments_map);
