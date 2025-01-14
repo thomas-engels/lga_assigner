7c40acc2d84fc90da7a600a2da5ed3391ffe7254
EggShark <eggshark@eggshark.dev>
2025-01-14 16:40:45 -0700

    clean up

--- main.cpp
+++ main.cpp
@@ -3,28 +3,9 @@
 #include <iomanip>
 #include <map>
 #include <set>
-#include <queue>
 #include <stdexcept>
 #include <string>
 #include <vector>
-
-void assign_questions(
-    const std::vector<int> &questions,
-    std::map<std::string, std::set<int>> &q_assignments_map,
-    const std::vector<std::string> &names
-) {
-    int num_questions = questions.size();
-    int num_names = names.size();
-    for (int i = 0; i < num_questions; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            const std::string& assigned_name = names[(i + j) % num_names];
-            q_assignments_map[assigned_name].insert(questions[i]);
-        }
-    }
-}
-
 
 void assign_questions_triple_approach(
     std::vector<int> &questions,
@@ -45,39 +26,8 @@ void assign_questions_triple_approach(
     }
 }
 
-
-
-void equalize_assignments(
-    const int question_count,
-    std::map<std::string, std::set<int>>& q_assignments_map
-) {
-    int max = 0;
-    for (const std::pair<std::string, std::set<int>>& p: q_assignments_map) {
-        max = std::max(max, (int)p.second.size());
-    }
-
-    for ( std::pair<const std::string, std::set<int>> &p: q_assignments_map) {
-        if (p.second.size() < max - 1) {
-            int last_q = 0;
-
-            for (const int& i : p.second) {
-                last_q = std::max(i, last_q);
-            }
-
-            int qs_to_assign = (max - 1) - p.second.size();
-            for (int i = 0; i < qs_to_assign; last_q++) {
-                if (p.second.insert((last_q % question_count) + 1).second) {
-                    i++;
-                }
-            }
-        }
-    }
-
-}
-
 void display_assignments( const std::map<std::string, std::set<int>> &q_assignments_map)
 {
-
     int longest_name = 0;
     for (const std::pair<std::string, std::set<int>> & name : q_assignments_map) {
         longest_name = std::max(longest_name, (int)name.first.size());
@@ -162,7 +112,6 @@ int main(int argc, char** arg_v)
         assign_questions_triple_approach(questions[i], q_assignments_map, names);
     }
 
-    //equalize_assignments(q_count, q_assignments_map);
 
     display_assignments(q_assignments_map);
 
